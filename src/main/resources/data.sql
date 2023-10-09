5412f0c89f60dcf787340bb8e3e3ddf02143ce46
ddu0422 <ddu0422@naver.com>
2023-10-09 15:55:49 +0900

    chore: data.sql을 이용하여 더미 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+insert into company (name, country, region)
+values ('원티드랩', '한국', '서울')
+;
+
+insert into company (name, country, region)
+values ('원티드코리아', '부산', '서울')
+;
+
+insert into company (name, country, region)
+values ('카카오', '한국', '판교')
+;
+
+insert into company (name, country, region)
+values ('네이버', '한국', '판교')
+;
+
+insert into member (name)
+values ('이두호')
+;
