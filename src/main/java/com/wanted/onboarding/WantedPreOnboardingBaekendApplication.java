542c1c7e816cac62bf23b22714f8d089edd90aff
ddu0422 <ddu0422@naver.com>
2023-10-09 14:47:15 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/wanted/onboarding/WantedPreOnboardingBaekendApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreOnboardingBaekendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedPreOnboardingBaekendApplication.class, args);
+    }
+
+}
