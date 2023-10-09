4f6f6daca776f2cb95fbfbb0f7dc3939e6412240
ddu0422 <ddu0422@naver.com>
2023-10-09 15:46:50 +0900

    feat: Member Model 생성

---
+++ src/main/java/com/wanted/onboarding/model/Member.java
@@ -0,0 +1,22 @@
+package com.wanted.onboarding.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", length = 200)
+    private String name;
+}
