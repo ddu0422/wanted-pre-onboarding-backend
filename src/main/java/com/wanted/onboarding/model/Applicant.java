40730b00e45da9340b9a34751f03d02e4d19a47e
ddu0422 <ddu0422@naver.com>
2023-10-11 21:37:45 +0900

    fix: Applicant Model에 GenerateValue Annotation 추가

--- src/main/java/com/wanted/onboarding/model/Applicant.java
+++ src/main/java/com/wanted/onboarding/model/Applicant.java
@@ -1,6 +1,9 @@
 package com.wanted.onboarding.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -13,6 +16,8 @@ import lombok.NoArgsConstructor;
 public class Applicant {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "applicant_id", nullable = false)
     private Long id;
 
     @ManyToOne
